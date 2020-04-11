fb4ce5d6fc7497d4bbf783b0d898df3f93e85beb
= <djnrrd@gmail.com>
2020-04-11 11:55:21 +0100

    Booted the stitches out to their own objects and brought all the property declarations back into __init__

--- twgamebook/game.py
+++ twgamebook/game.py
@@ -1,8 +1,52 @@
 import logging
 import json
-
+from textwrap import wrap
 # Get the log into this namespace
 logger = logging.getLogger('twgamebook')
+
+class twgbStitch(object):
+    """An object for managing the individual stitches of the story
+    """
+
+    def __init__(self, key, stitch):
+        """Generate a twgbSwitch object from the source JSON
+
+        :param key: The switch key from the source JSON
+        :type key: str
+        :param stitch: The switch data
+        :type stitch: dict
+        """
+        logger.debug(f"Building switch obect {key}")
+        self.key = key
+        self.content = stitch['content'][0]
+        self.divert = ''
+        self.options = []
+        self.flagName = ''
+        self.pageNum = 0
+        self.pageLabel = ''
+        options_list = [x for x in stitch['content'] if isinstance(x, dict)]
+        for option in options_list:
+            if 'divert' in option:
+                logger.debug(f"Adding divert to {option['divert']}")
+                self.divert = option['divert']
+            if 'option' in option:
+                logger.debug(f"Adding options {option}")
+                self.options.append(option)
+            if 'flagName' in option:
+                logger.debug(f"Adding flag {option['flagName']}")
+                self.flagName = option['flagName']
+            if 'pageNum' in option:
+                logger.debug(f"Adding pageNum {option['pageNum']}")
+                self.pageNum = option['pageNum']
+            if 'pageLabel' in option:
+                logger.debug(f"Adding pageLabel {option['pageLabel']}")
+                self.pageLabel = option['pageLabel']
+
+    def __repr__(self):
+        return self.key
+
+    def __str__(self):
+        return self.key
 
 class twGameBook(object):
     """An object for loading JSON files from inklewriter.com
@@ -15,26 +59,38 @@ class twGameBook(object):
         :type source_file: str"""
         if isinstance(source, str):
             if source[0:7] == 'http://':
-                self.get_http_json(source)
+                source_data = self.load_http_json(source)
             else:
-                self.get_local_json(source)
+                source_data = self.load_local_json(source)
+            if 'title' and 'data' in source_data:
+                self.title = source_data['title']
+                self.initial = source_data['data']['initial']
+                self.stitches = self.load_stitches(source_data['data'][
+                                                      'stitches'])
+                self.flags = []
+            else:
+                logger.warning('Did not find expected Inklewriter JSON object')
+                raise ValueError('Expected Inklewriter JSON object')
         else:
             raise KeyError('Expected string object as source')
 
-    def get_http_json(self, source_url):
+    def load_http_json(self, source_url):
         """Get the source file from the internet
 
         :param source_url:
         :type source_url: str
+        :return: Parsed JSON object
         """
         logger.debug(f"{source_url} provided as URL")
         logger.warning('Currently does not support HTTP')
+        return False
 
-    def get_local_json(self, source_file):
+    def load_local_json(self, source_file):
         """Get the source file from local disk
 
         :param source_file: Path to the locally stored source file
         :type source_file: str
+        :return: The parsed JSON object
         """
         try:
             logger.debug(f"Attempting to open {source_file}")
@@ -48,55 +104,48 @@ class twGameBook(object):
         except json.JSONDecodeError:
             logger.warning(f"Could not parse JSON from {source_file}")
             raise json.JSONDecodeError()
-        self.load_game(source_json)
+        return source_json
 
-    def load_game(self, source_data):
-        """Load the game into memory
-        :param source_data: Source data parsed through json library
-        :type source_data: dict
+    def load_stitches(self, stitches):
+        """Generate a list of twgbStitch objects
+
+        :param stitches: Dictionary of stitches from the JSON source file
+        :type stitches: dict
+        :return: List of twgbSwitch objects
+        :rtype: list
         """
-        # Final check for the keys and then load everything into the object.
-        if 'title' and 'data' in source_data:
-            self.title = source_data['title']
-            self.initial = source_data['data']['initial']
-            self.stitches = source_data['data']['stitches']
-            self.flags = []
-        else:
-            logger.warning('Did not find expected Inklewriter JSON object')
-            raise ValueError('Expected Inklewriter JSON object')
+        ret_list = []
+        for key in stitches:
+            ret_list.append(twgbStitch(key, stitches[key]))
+        return ret_list
 
-    def read_section(self, start='', ret_str=''):
+    def get_stitch(self, key):
+        stitch = [x for x in self.stitches if x.key == key]
+        return stitch.pop()
+
+    def read_section(self, start_key='', ret_list=[]):
         """Read a section of the game until options or an ending is found
 
-        :param start: The key for the starting stitch of the story. Leaving
+        :param start_key: The key for the starting stitch of the story. Leaving
             this blank will start the story from the beginning
-        :type start: str
-        :param ret_str: The cumulative previously returned string, used in
+        :type start_key: str
+        :param ret_list: The cumulative previously returned thread, used in
             recursion
-        :type ret_str: str
-        :return: The complete section of this game
-        :rtype: str
+        :type ret_list: list
+        :return: A list of tweets made up of all the stitches for this section.
+        :rtype: list
         """
-        if not start:
-            start = self.initial
-        logger.debug(f"using Stitch ID {start}")
-        ret_str += self.stitches[start]['content'][0]
-        # Get the keys of the dict objects and their index position in the
-        # content list, a bit messy but a one off
-        stitch_keys = {}
-        for content in self.stitches[start]['content']:
-            if isinstance(content, dict):
-                for content_key in content:
-                    stitch_keys[content_key] = self.stitches[start][
-                                                   'content'].index(content)
-        logger.debug(f"Found the following keys: {stitch_keys}")
-        # Now look if we need to keep going to the next piece
-        if 'divert' in stitch_keys:
-            next = self.stitches[start]['content'][stitch_keys['divert']][
-                'divert']
-            ret_str += '\n\n'
-            return self.read_section(next, ret_str)
-        elif 'option' in stitch_keys:
+        if not start_key:
+            start_key = self.initial
+        logger.debug(f"using Stitch ID {start_key}")
+        stitch = self.get_stitch(start_key)
+        ret_list += wrap(stitch.content, 280)
 
+        # Now look if we need to keep going to the next piece
+        if stitch.divert:
+            return self.read_section(stitch.divert, ret_list)
+        elif stitch.options:
+            return ret_list
         else:
-            return ret_str
+            return ret_list
+
