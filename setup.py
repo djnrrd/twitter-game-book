833cf2c61abff8d54179796730abf4ee8c133674
= <djnrrd@gmail.com>
2020-04-23 21:15:06 +0100

    Pausing while experimenting with setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,24 @@
 from setuptools import setup, find_packages
 setup(
-    name="twitter-game-book",
-    version="0.0.1",
+    name='twitter-game-book',
+    version='0.0.1',
     packages=find_packages(),
+    # Register the command line package as a console-script to go in $PATH
+    entry_points = {
+        'console_scripts': ['runtwgb=twgamebook.command_line:main'],
+    },
+    author='DJ Nrrd',
+    author_email='djnrrd@gmail.com',
+    url='https://github.com/djnrrd/twitter-game-book',
+    project_urls={
+        'Bug Tracker': 'https://github.com/djnrrd/twitter-game-book/issues',
+        'Source Code': 'https://github.com/djnrrd/twitter-game-book/',
+    },
+    keywords='twitter bot twitterbot games',
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: No Input/Output (Daemon)',
+        'Topic :: Games/Entertainment :: Role-Playing'
+    ]
 )
