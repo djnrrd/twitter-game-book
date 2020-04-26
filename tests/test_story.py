6d77b81f86df3eb74ad171e2ddf793032c9ff8f1
= <djnrrd@gmail.com>
2020-04-26 13:15:59 +0100

    Built basic tests layout and added it to MANIFEST.in, moved the test_inputs folder in there.  Skeleton test file built by PyCharm

---
+++ tests/test_story.py
@@ -0,0 +1,30 @@
+from unittest import TestCase
+
+
+class TestTWGBStory(TestCase):
+    def test__load_http_json(self):
+        self.fail()
+
+    def test__load_local_json(self):
+        self.fail()
+
+    def test__load_stitches(self):
+        self.fail()
+
+    def test__get_stitch(self):
+        self.fail()
+
+    def test__get_options(self):
+        self.fail()
+
+    def test__pass_conditions(self):
+        self.fail()
+
+    def test_get_section(self):
+        self.fail()
+
+    def test_get_hashtags(self):
+        self.fail()
+
+    def test_set_flags(self):
+        self.fail()
